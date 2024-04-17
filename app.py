9957f94c2425396fa5fe98373dfba4171ab8c7bf
Deepak Nigam <deepak.nigam@pepcus.com>
2024-04-17 17:56:46 +0530

    Added new API for testing.

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ app = FastAPI()
 
 @app.get('/')
 def hello_world():
-    return "Hello, World"
+    return "Hello,World"
 
+@app.get("/responses/{response_id}")
+async def get_response(response_id):
+    return {"response_id": response_id}
 
 if __name__ == '__main__':
     import uvicorn
