d611b66b498894e4d924a2f01c6fd4587d3c5577
Deepak Nigam <deepak.nigam@pepcus.com>
2024-04-27 17:38:15 +0530

    Added support for OCR.

--- utils_langchain_chat.py
+++ utils_langchain_chat.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 from utils_langchain_pinecone import get_pinecone_query_engine
 from utils_langchain_sql import get_sql_query_agent
+from utils_langchain_ocr import read_pdf_through_ocr
 
 client = OpenAI()
 
@@ -25,8 +26,9 @@ SYSTEM_PROMPT = '''
     You are an AI-powered chatbot specialized in providing information on the HR domain.
     If the user query pertains to company, HR policies, employee handbook, or work culture, call the function `get_information_from_employee_handbook` and pass user's query without any modification to it.
     If the user query relates to data such as the number of employees, departments, or salary information, call the function `get_information_from_application_database` and pass user's query without any modification to it.
+    If the user asks for PDF reconciliation call the function `reconcile_pdf_through_ocr`and pass filelocation without any modification to it.
     Do not make up anything from your end.
-'''  
+'''
 
 tools = [
     {
@@ -62,6 +64,23 @@ tools = [
                 "required": ["query"],
             },
         }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "reconcile_pdf_through_ocr",
+            "description": "Reconcile PDF through OCR",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "filelocation": {
+                        "type": "string",
+                        "description": "File Location",
+                    }
+                },
+                "required": ["filelocation"],
+            },
+        }
     }
 ]   
 
@@ -71,15 +90,17 @@ initial_messages = [{"role": "system", "content": SYSTEM_PROMPT}]
 def execute_function_call(pc, db, llm, embeddings, company, message):
     print("========inside execute_function_call=============")
     if message.tool_calls[0].function.name == "get_information_from_employee_handbook":
-        print("========pinecone=============")
         query = json.loads(message.tool_calls[0].function.arguments)["query"]
         print("========pinecone query=============", query)
         results = get_pinecone_query_engine(pc, llm, embeddings, company, query)
     elif message.tool_calls[0].function.name == "get_information_from_application_database":
-        print("========get_sql_query_agent=============")
         query = json.loads(message.tool_calls[0].function.arguments)["query"]
         print("========sql query=============", query)
         results = get_sql_query_agent(db, llm, query)
+    elif  message.tool_calls[0].function.name == "reconcile_pdf_through_ocr":
+        filelocation = json.loads(message.tool_calls[0].function.arguments)["filelocation"]
+        print("========filelocation=============", filelocation)
+        results = read_pdf_through_ocr(filelocation)
     else:
         results = f"Error: function {message.tool_calls[0].function.name} does not exist"
     return results
@@ -95,6 +116,9 @@ def get_chat_response(pc, db, llm, embeddings, company, query):
     assistant_message = chat_response.choices[0].message
     print("========inside get response assistant_message=============", assistant_message)
     if assistant_message.tool_calls:
-         results = execute_function_call(pc, db, llm, embeddings, company, assistant_message)
-    print("========inside get response results=============", results) 
-    return results
+        response = execute_function_call(pc, db, llm, embeddings, company, assistant_message)
+    else:
+        response = {}
+        response['output'] =  assistant_message.content    
+    print("========inside get response results=============", response) 
+    return response
---
+++ utils_langchain_ocr.py
@@ -0,0 +1,4 @@
+
+def read_pdf_through_ocr(filelocation):
+    print("====going to process file from the location ========", filelocation)
+    return "Work in progress"
