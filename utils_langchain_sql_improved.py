f7e86075f717d0913c326f685637bf72f9927599
Deepak Nigam <deepak.nigam@pepcus.com>
2024-04-29 16:26:08 +0530

    Added missing property.

--- utils_langchain_sql_improved.py
+++ utils_langchain_sql_improved.py
@@ -6,7 +6,7 @@ import os
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 db = SQLDatabase.from_uri(os.environ['PG_DB_URI'])
 
-agent_executor = create_sql_agent(llm, db, agent_type="openai-tools")
+agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools")
 
 def get_sql_query_agent(query):
     return agent_executor.invoke(query)
