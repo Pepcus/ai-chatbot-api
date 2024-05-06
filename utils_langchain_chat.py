f53896411419041962e24518b9442fd014a61ac1
Deepak Nigam <deepak.nigam@pepcus.com>
2024-05-06 16:38:58 +0530

    Removed tool_choice parameter since the default value is set as auto.

--- utils_langchain_chat.py
+++ utils_langchain_chat.py
@@ -43,13 +43,12 @@ tools = [
 
 
 @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
-def chat_completion_request(messages, tools=None, tool_choice=None, model='gpt-3.5-turbo'):
+def chat_completion_request(messages, tools=None, model='gpt-3.5-turbo'):
     try:
         response = client.chat.completions.create(
             model=model,
             messages=messages,
-            tools=tools,
-            tool_choice=tool_choice,
+            tools=tools
         )
         return response
     except Exception as e:
