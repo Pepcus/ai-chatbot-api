22e6f44cee46caca52b4d2dea1bf242674211dc5
Deepak Nigam <deepak.nigam@pepcus.com>
2024-05-17 13:30:12 +0530

    Removed unrequired changes as per SQL functionality.

--- config/config.py
+++ config/config.py
@@ -2,7 +2,6 @@ from langchain_openai import OpenAIEmbeddings
 from pinecone.grpc import PineconeGRPC
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain_community.utilities import SQLDatabase
 from openai import OpenAI
 import os
 
@@ -18,13 +17,7 @@ openai_llm = ChatOpenAI(
         model_name='gpt-3.5-turbo',  
         temperature=0.0  
     )
-pg_database = SQLDatabase.from_uri(os.environ['PG_DB_URI'])
 gcp_bucket_name = os.environ['GCP_BUCKET_NAME']
-pg_db_uri = os.environ['PG_DB_URI']
 api_client_id = os.environ['API_CLIENT_ID']
 api_client_secret = os.environ['API_CLIENT_SECRET']
 local_download_path = os.environ['LOCAL_DOWNLOAD_PATH']
-SQL_SYSTEM_PROMPT = os.environ['SQL_SYSTEM_PROMPT']
-SQL_ANSWER_PROMPT = os.environ['SQL_ANSWER_PROMPT']
-DB_SCHEMA = os.environ['DB_SCHEMA_INDEX_NAME'] 
-DB_SCHEMA_QUERY = os.environ['DB_SCHEMA_QUERY']
