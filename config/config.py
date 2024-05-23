da70d3c308478a2744f92942b76e96f517c21ede
Deepak Nigam <deepak.nigam@pepcus.com>
2024-05-23 17:00:49 +0530

    Added config.py file.

--- config/config.py
+++ config/config.py
@@ -12,6 +12,9 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from openai import OpenAI
 import os
+import json
+import tempfile
+import ast
 
 load_dotenv()
 
@@ -35,3 +38,25 @@ LANGCHAIN_API_KEY = os.environ['LANGCHAIN_API_KEY']
 LANGCHAIN_TRACING_V2 = os.environ['LANGCHAIN_TRACING_V2']
 LANGCHAIN_ENDPOINT= os.environ['LANGCHAIN_ENDPOINT'] 
 LANGCHAIN_PROJECT = os.environ['LANGCHAIN_PROJECT'] 
+
+gcp_json = {
+  "type": os.environ['GCP_TYPE'],
+  "project_id": os.environ['GCP_PROJECT_ID'],
+  "private_key_id": os.environ['GCP_PRIVATE_KEY_ID'],
+  "private_key": os.environ['GCP_PRIVATE_KEY'],
+  "client_email": os.environ['GCP_CLIENT_EMAIL'],
+  "client_id": os.environ['GCP_CLIENT_ID'],
+  "auth_uri": os.environ['GCP_AUTH_URI'],
+  "token_uri": os.environ['GCP_TOKEN_URI'],
+  "auth_provider_x509_cert_url": os.environ['GCP_AUTH_PROVIDER_X509_CERT_URL'],
+  "client_x509_cert_url": os.environ['GCP_CLIENT_X509_CERT_URL'],
+  "universe_domain": os.environ['GCP_UNIVERSE_DOMAIN']
+}
+
+# Create a temporary file to write the credentials JSON
+with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.json') as temp_file:
+    json.dump(gcp_json, temp_file)
+    temp_file_path = temp_file.name
+
+# Set the GOOGLE_APPLICATION_CREDENTIALS environment variable to the path of the temporary file
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = temp_file_path
